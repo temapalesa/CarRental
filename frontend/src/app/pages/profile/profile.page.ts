74cb59c9e0a2974979515fdc0b0edf323a30a082
temapalesa <37109819+temapalesa@users.noreply.github.com>
2023-03-29 12:35:06 +0200

    profile

--- frontend/src/app/pages/profile/profile.page.ts
+++ frontend/src/app/pages/profile/profile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +9,27 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  isLoggedIn: boolean = true;
 
-  constructor(private router :Router) { }
+  isLoggedIn: boolean = true;
+  users :any ;
+
+  EditUserForm:FormGroup = new FormGroup({
+    address:new FormControl(''),
+    phonenumber:new FormControl(''),
+  });
+   
+
+  constructor(private router :Router, private authservice : UsersService) { }
 
   ngOnInit() {
+
+    this.authservice.getUsers().subscribe((res:any) => {
+      let result = res;
+      
+      this.users = result.filter((res:any) => String(res.email) === String(sessionStorage.getItem('loggedEmail')))
+      console.log(this.users)
+    })
+
   }
 
   logOut()
